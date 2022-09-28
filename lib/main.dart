3473c6b29fec1bd689bcc20c8465492caab914c1
Mohibkhizer <mohibkhizer733@gmail.com>
2022-09-28 16:10:07 +0500

    list_builoder

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application_15/home_page.dart';
-import 'package:flutter_application_15/list_builder_screen.dart';
+import 'package:flutter_application_15/catagory_screen.dart';
 import 'package:video_player/video_player.dart';
 
 void main() {
--- lib/models/catagory_model.dart
+++
@@ -1,5 +0,0 @@
-import 'package:flutter/cupertino.dart';
-
-class CatagoryModel {
-  CatagoryModel((required this.catcolor, required this.catName));
-}
